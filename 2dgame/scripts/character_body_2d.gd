170712bcffaae91f5ac247d2a7eb0e1ad3060c95
Sammy <sambolein@gmail.com>
2024-11-03 20:39:40 +0100

    Init

--- 2dgame/scripts/character_body_2d.gd
+++ 2dgame/scripts/character_body_2d.gd
@@ -1,20 +1,23 @@
 extends CharacterBody2D
 
-
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-
+const SPEED = 100.0
 
 func _physics_process(event):
 	var directionX := Input.get_axis("ui_left", "ui_right")
 	var directionY := Input.get_axis("ui_up", "ui_down")
+	var sprint = 1
+	if Input.is_key_pressed(KEY_SHIFT):
+		sprint = 2
+	#if x input -> increase x velocity and slow down y velocity to disable diagonal movement
 	if directionX:
-		velocity.x = directionX * SPEED
+		velocity.x = directionX * SPEED * sprint 
+		velocity.y = move_toward(velocity.y, 0, SPEED)
+	#if y input -> increase y velocity and slow down x velocity to disable diagonal movement
+	elif directionY:
+		velocity.y = directionY * SPEED * sprint
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+	#if no input, slow velocity down
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-	if directionY:
-		velocity.y = directionY * SPEED
-	else:
 		velocity.y = move_toward(velocity.y, 0, SPEED)
-
 	move_and_slide()
