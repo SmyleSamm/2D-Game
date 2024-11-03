16ead97567216f43f506252e1ee7f541b3fea410
Sammy <sambolein@gmail.com>
2024-11-03 22:51:10 +0100

    char speed rebalance and added running

--- 2dgame/scripts/character_body_2d.gd
+++ 2dgame/scripts/character_body_2d.gd
@@ -1,21 +1,27 @@
 extends CharacterBody2D
 
-const SPEED = 100.0
+const SPEED = 80.0
+var RUNNING = 1.3
 
 func _physics_process(event):
 	var directionX := Input.get_axis("ui_left", "ui_right")
 	var directionY := Input.get_axis("ui_up", "ui_down")
-	var sprint = 1
+	var run = 1
+	
+	#if shift input -> increase running spead by var RUNNING
 	if Input.is_key_pressed(KEY_SHIFT):
-		sprint = 2
+		run = RUNNING
+		
 	#if x input -> increase x velocity and slow down y velocity to disable diagonal movement
 	if directionX:
-		velocity.x = directionX * SPEED * sprint 
+		velocity.x = directionX * SPEED * run
 		velocity.y = move_toward(velocity.y, 0, SPEED)
+		
 	#if y input -> increase y velocity and slow down x velocity to disable diagonal movement
 	elif directionY:
-		velocity.y = directionY * SPEED * sprint
+		velocity.y = directionY * SPEED * run
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+		
 	#if no input, slow velocity down
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
